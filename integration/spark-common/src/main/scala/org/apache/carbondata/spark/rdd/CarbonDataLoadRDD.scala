d0a82aadd1c9db49dc3122da8bfa2faa9616b13b
jackylk <jacky.likun@huawei.com>
2016-11-29 19:48:11 +0800

    rebase

--- integration/spark-common/src/main/scala/org/apache/carbondata/spark/rdd/CarbonDataLoadRDD.scala
+++ integration/spark-common/src/main/scala/org/apache/carbondata/spark/rdd/CarbonDataLoadRDD.scala
@@ -27,8 +27,9 @@ import scala.util.Random
 
 import org.apache.spark.{Partition, SerializableWritable, SparkContext, SparkEnv, TaskContext}
 import org.apache.spark.annotation.DeveloperApi
-import org.apache.spark.rdd.RDD
+import org.apache.spark.rdd.{DataLoadCoalescedRDD, DataLoadPartitionWrap, RDD}
 import org.apache.spark.sql.Row
+import org.apache.spark.util.SparkUtil
 
 import org.apache.carbondata.common.logging.LogServiceFactory
 import org.apache.carbondata.common.logging.impl.StandardLogService
@@ -555,7 +556,7 @@ class PartitionIterator(partitionIter: Iterator[DataLoadPartitionWrap[Row]],
         context)
   }
   def initialize: Unit = {
-    TaskContextUtil.setTaskContext(context)
+    SparkUtil.setTaskContext(context)
   }
 }
 /**
@@ -590,7 +591,7 @@ class RddIterator(rddIter: Iterator[Row],
   }
 
   def initialize: Unit = {
-    TaskContextUtil.setTaskContext(context)
+    SparkUtil.setTaskContext(context)
   }
 
 }
